4daa9ec3160c8cf1bc75e5d7030621a7169a24b1
유일송 <ilsong963@naver.com>
2019-06-06 18:43:11 +0900

    show new window

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import tkinter.font
-import tkinter
+from tkinter import *
 
-def disabled():
-    button1.config(state='disabled')
-    button2.config(state='disabled')
-    button3.config(state='disabled')
-
-def normal():
+def quit():
     button1.config(state='normal')
-    button2.config(state='normal')
-    button3.config(state='normal')
 
-def beginner():
-    problem = tkinter.Tk()
-    window.protocol("WM_DELETE_WINDOW", normal)
-    disabled()
+    newwin.destroy()
 
-def Intermediate():
-    problem = tkinter.Tk()
-    window.protocol("WM_DELETE_WINDOW", normal)
-    disabled()
+def disable_event():
+    pass
 
-def Advanced():
-    problem = tkinter.Tk()
-    window.protocol("WM_DELETE_WINDOW", normal)
-##########
-    #disabled()
+def new_winF(): # new window definition
+
+    global button1
+    button1.config(state='disabled')
+
+    global newwin
+    newwin = Toplevel(window)
+    newwin.geometry("300x300")
+    newwin.resizable(False, False)
+
+    font = tkinter.font.Font(family="맑은 고딕", size=20)
+    label = tkinter.Label(newwin, text="문제를 풀어보세요!!", width=30, height=2, fg="red", font=font)
+    label.pack()
+    btn1 = tkinter.Button(newwin, overrelief="solid", width=30, height=2, repeatdelay=1000, repeatinterval=100, text = "종료", command=quit)
+    btn1.place(x=40, y=250)
+    newwin.protocol("WM_DELETE_WINDOW", disable_event)
+
+
 
 
 window = tkinter.Tk()
@@ -37,20 +38,9 @@ font = tkinter.font.Font(family="맑은 고딕", size=20)
 label = tkinter.Label(window, text="문제를 풀어보세요!!", width=30, height=2, fg="red", font=font)
 label.pack()
 
-button1 = tkinter.Button(window, overrelief="solid", width=30, height=2, repeatdelay=1000, repeatinterval=100, text="초급"
-                         , command=beginner)
-button1.pack(pady = 10)
+button1 = tkinter.Button(window, overrelief="solid", width=30, height=2, repeatdelay=1000, repeatinterval=100, text="시작"
+                         , command=new_winF)
+button1.config(state='normal')
 
-button2 = tkinter.Button(window, overrelief="solid", width=30, height=2, repeatdelay=1000, repeatinterval=100, text="중급"
-                         , command=Intermediate)
-button2.pack(pady = 10)
-
-button3 = tkinter.Button(window, overrelief="solid", width=30, height=2, repeatdelay=1000, repeatinterval=100, text="고급"
-                         , command=Advanced)
-button3.pack(pady = 10)
-
-
+button1.pack()
 window.mainloop()
-
-if(window.destroy()):
-    normal()
